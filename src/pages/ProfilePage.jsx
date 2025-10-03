410af1b58d84d11daf8dc3b31c7344e7304ffaee
Jakaria Ahmed <jakaria88520@gmail.com>
2025-10-03 12:40:53 +0600

    connected with backend, create Context function, user authentication and authorization,user signup & signin, logout

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,23 +1,40 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import assets from "../assets/assets";
+import { AuthContext } from "../../store/AuthStore";
 
 const ProfilePage = () => {
+  const { authUser, updateProfile } = useContext(AuthContext);
+
   const [selectedImg, setSelectedImg] = useState(null);
   const navigate = useNavigate();
-  const [name, setName] = useState("John Doe");
-  const [bio, setBio] = useState("Hi there, I am using BlinkChat");
+  const [name, setName] = useState(authUser.fullName);
+  const [bio, setBio] = useState(authUser.bio);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!selectedImg) {
+      await updateProfile({ fullName: name, bio });
+      navigate("/");
+      return;
+    }
 
-    navigate("/")
-  }
+    const reader = new FileReader();
+    reader.readAsDataURL(selectedImg);
+    reader.onload = async () => {
+      const base64Image = reader.result;
+      await updateProfile({ profilePic: base64Image, fullName: name, bio });
+      navigate("/");
+    };
+  };
 
   return (
     <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
       <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
-        <form onSubmit={handleSubmit} className="flex flex-col gap-5 p-10 flex-1">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-5 p-10 flex-1"
+        >
           <h3 className="text-lg"> Profile Details</h3>
           <label
             htmlFor="avatar"
@@ -60,11 +77,18 @@ const ProfilePage = () => {
             rows={4}
           ></textarea>
 
-          <button type="submit" className="bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer">
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer"
+          >
             Save
           </button>
         </form>
-        <img className="max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10" src={assets.logo_icon} alt="" />
+        <img
+          className={`max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10 ${selectedImg && 'rounded-full'}`}
+          src={authUser?.profilePic || assets.logo_icon}
+          alt=""
+        />
       </div>
     </div>
   );
