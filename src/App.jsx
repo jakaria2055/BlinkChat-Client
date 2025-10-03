8f373a66f2728bd27a8522f04b082352ac4f633b
Jakaria Ahmed <jakaria88520@gmail.com>
2025-10-03 22:45:45 +0600

    bg solve

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { AuthContext } from "../store/AuthStore";
 const App = () => {
   const {authUser} = useContext(AuthContext)
   return (
-    <div className="bg-[url('./assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster />
       <Routes>
         <Route path='/' element={authUser ? <HomePages /> : <Navigate to={"/login"} />} />
